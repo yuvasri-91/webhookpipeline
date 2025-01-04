f829c68c5c77d7c9337ee7df125947a31bc151cc
yuva <yuva@gmail.com>
2025-01-04 22:20:04 +0530

    make changes in file

---
+++ test.java
@@ -0,0 +1,15 @@
+public class Test {
+
+    static void add()
+    {
+        int a=50;
+        int b=10;
+        int c=50;
+        int res=a+b+c;
+        System.out.println("the result is " +res);
+    }
+    public static void main(String[] args)
+    {
+        add();
+    }
+}
